a34055a8391f2f6a0291510cda72e399a956ba8a
aap9002 <aap9002@gmail.com>
2023-02-09 13:28:27 +0000

    express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const util = require('util');
+const port = process.env.PORT || 5000;
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(express.static('client/build'))
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+});
