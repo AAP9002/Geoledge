3b176ff298f8cb2756ffd18375f507e42591c54d
Ibrahim <ibrahim.elbehisy@student.manchester.ac.uk>
2023-02-14 07:59:35 +0000

    Ibra's sign up pages

--- client/src/Pages/Sign-up.jsx
+++ client/src/Pages/Sign-up.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Signup.css';
 
+const SignUpPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
-const About = () => {
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Name:', name, 'Email:', email, 'Password:', password);
+  };
+
   return (
-    <div>
-          <div>
-            <p>
-              Login/Sign-up Page
-            </p>
-          </div>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form" id="signup-form">
+        <h1>Sign Up</h1>
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+        <p>Already have an account? <a href="Sign_up">Log In</a></p>
+      </form>
     </div>
- 
   );
 };
 
-export default About;
+export default SignUpPage;
