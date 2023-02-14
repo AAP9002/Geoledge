4fd320226faaa6d740e7134860e88026e84f4af5
aap9002 <aap9002@gmail.com>
2023-02-14 18:04:42 +0000

    set connection to sql

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require('express');
+const mysql = require('mysql')
 const app = express();
 const port = process.env.PORT || 5000;
+const connection = mysql.createConnection({
+  host: process.env.DB_SERVER,
+  user: process.env.DBUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DBNAME
+});
 
 // copy .env.TEMPLATE into a new file called .env (this file will not be send to git, so real passwords can be set to the .env)
 
@@ -11,22 +18,13 @@ console.log(process.env.SECRET_CODE_EXAMPLE);
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+//point to ststic react files
 app.use(express.static('client/build'))
 
-
-
-console.log(process.env.DBNAME)
-
-// create a GET route
+// test if can get a record from mysql database and return json
 app.get('/express_backend', (req, res) => {
-  const mysql = require('mysql')
-  const connection = mysql.createConnection({
-    host: process.env.DB_SERVER,
-    user: process.env.DBUSER,
-    password: process.env.PASSWORD,
-    database: process.env.DBNAME
-  });
   connection.connect();
+
   connection.query("select * from temp", function(error, results){
     console.log(results);
     res.send({  results  });
@@ -34,5 +32,3 @@ app.get('/express_backend', (req, res) => {
 
   connection.end()
 });
-
-// Adding a new user to the database
