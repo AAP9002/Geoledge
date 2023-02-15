019b7b4a684d988e57bfedd390ff6c54b2c25001
aap9002 <aap9002@gmail.com>
2023-02-15 10:37:50 +0000

    Improved country data API and cleaned data (marked as unknown if data doesn't exist)

--- server.js
+++ server.js
@@ -67,11 +67,32 @@ function GenerateCountryDataTable(response){
     // NOTE some countries don't have capitals
     var capitalName = "No Capital"
     if(countries[i].capital == undefined){
-      console.log(countries[i].name.common)
+      //console.log(countries[i].name.common)
     }
     else{
       capitalName = countries[i].capital[0]
     }
+
+    //LANGUAGE
+    //seems to be only Antarctica
+    var langName = "No Official Language"
+    if(countries[i].languages == undefined){
+      console.log(countries[i].name.common)
+    }
+    else{
+      langName = Object.entries(countries[i].languages)[0][1]
+    }
+
+    //Currency
+    //Antarctica, Bouvet Island and Heard Island and McDonald Islands don't have an official currency
+    var currencySymbol = "Unknown Currency"
+    if(countries[i].currencies == undefined){
+      console.log(countries[i].name.common)
+    }
+    else{
+      currencySymbol = Object.entries(countries[i].currencies)[0][1].symbol
+    }
+
     //clean and make country object
     var individualCountry = {
       name: countries[i].name.common,
@@ -82,10 +103,13 @@ function GenerateCountryDataTable(response){
       lng: countries[i].latlng[1],
       surface_area: countries[i].area,
       population: countries[i].population,
+      timezone:Object.entries(countries[i].timezones)[0][1],
       driving_side: countries[i].car.side,
       capital: capitalName,
-      flag:countries[i].flags.svg
-
+      flag:countries[i].flags.svg,
+      language: langName,
+      map: "https://maps.google.com/maps?q="+countries[i].name.common.replaceAll(" ","+")+"+country&amp;t=&amp;ie=UTF8&amp;iwloc=&amp;output=embed",
+      currency: currencySymbol
     }
 
     formattedCountries.push(individualCountry) // add country to countries object
