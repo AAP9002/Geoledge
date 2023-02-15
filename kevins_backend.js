2818b326ca25995b0893b5c2eedeb9432f22ee2e
brickchairduck <kevinyeeli004@gmail.com>
2023-02-15 18:42:10 +0000

    adding kevs_backend import route to server js

--- kevins_backend.js
+++ kevins_backend.js
@@ -1,3 +1,8 @@
 // split backend into multiple files to allow simultaneously synchroniz-
 // ed streamlined work: 
 // https://stackoverflow.com/questions/33946972/how-to-split-node-js-files-in-several-files/33947204#33947204
+module.exports = function (app) {
+    app.get('/kevin', function(req, res) {
+        res.json(200, {'test': 'it works!'})
+    })
+}
