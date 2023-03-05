5c296fc2c2dbeaae767279b43300282e61612bfe
aap9002 <aap9002@gmail.com>
2023-03-05 14:35:34 +0000

    Change syntax to try hosting on render

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./Home.css";
-import { Link } from 'react-router-dom';
 
 
 const About = () => {
@@ -9,20 +8,20 @@ const About = () => {
     <div>
 
     </div>
-    <div class ="column">
-      <div class ="top">
-      <div class ="row-md-8">
+    <div className ="column">
+      <div className ="top">
+      <div className ="row-md-8">
         <h1>GEOLEDGE</h1>
         <h2>Prepared to test your geographical knowledge?</h2>
-        <div class = "btns">
-        <a class ="styledbutton" href='./Play'> Host Game</a>
-        <a class ="styledbutton" href='./Play'> Play Online</a>
-        <a class ="styledbutton" href='./Play'> Join Game</a>
+        <div className = "btns">
+        <a className ="styledbutton" href='./Play'> Host Game</a>
+        <a className ="styledbutton" href='./Play'> Play Online</a>
+        <a className ="styledbutton" href='./Play'> Join Game</a>
         </div>
       </div>
       </div>
-      <div class = "bottom">
-      <div class ="row-md-12">
+      <div className = "bottom">
+      <div className ="row-md-12">
         <h1>What Is Geoledge?</h1>
         <p>Geoledge is an online quiz-type game.</p>
         <p>Each round you will be given a list of facts describing a mystery country.</p>
--- client/src/Pages/Play/Play.jsx
+++ client/src/Pages/Play/Play.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import button from 'react-bootstrap/button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import "./Play.css";
 import { Link } from 'react-router-dom';
@@ -26,12 +25,12 @@ function Play(){
   };
   return (
   <div >
-  <div class="lob">
+  <div className="lob">
   <h1>LOBBY</h1>
   </div>
-  <div class="row">
-    <div class="col-md-8">
-    <table class = "table">
+  <div className="row">
+    <div className="col-md-8">
+    <table className = "table">
         <thead> <tr> <th>no:</th><th>Player</th></tr></thead>
         <tbody>
           <tr><td>{No}</td></tr>
@@ -39,33 +38,33 @@ function Play(){
 
       </table>
     </div>
-    <div class="col-md-4">
-      <div class="settingscontainer">
+    <div className="col-md-4">
+      <div className="settingscontainer">
 
-        <div class="settings">
-          <h2 class="h2"> Settings </h2>
+        <div className="settings">
+          <h2 className="h2"> Settings </h2>
           <div>
             
               <h3>Number Of Rounds</h3>
               <ButtonGroup> 
-                <button class="styledbutton2" id="1" onClick={changeNOR}>1</button><button class="styledbutton2" id="2" onClick={changeNOR}>2</button><button class="styledbutton2" id="3" onClick={changeNOR}>3</button>
-                <button class="styledbutton2" id="4" onClick={changeNOR}>4</button><button class="styledbutton2" id="5" onClick={changeNOR}>5</button><button class="styledbutton2" id="6" onClick={changeNOR}>6</button>
-                <button class="styledbutton2" id="7" onClick={changeNOR}>7</button><button class="styledbutton2" id="8" onClick={changeNOR}>8</button><button class="styledbutton2" id="9" onClick={changeNOR}>9</button>
-                <button class="styledbutton2" id="10" onClick={changeNOR}>10</button>
+                <button className="styledbutton2" id="1" onClick={changeNOR}>1</button><button className="styledbutton2" id="2" onClick={changeNOR}>2</button><button className="styledbutton2" id="3" onClick={changeNOR}>3</button>
+                <button className="styledbutton2" id="4" onClick={changeNOR}>4</button><button className="styledbutton2" id="5" onClick={changeNOR}>5</button><button className="styledbutton2" id="6" onClick={changeNOR}>6</button>
+                <button className="styledbutton2" id="7" onClick={changeNOR}>7</button><button className="styledbutton2" id="8" onClick={changeNOR}>8</button><button className="styledbutton2" id="9" onClick={changeNOR}>9</button>
+                <button className="styledbutton2" id="10" onClick={changeNOR}>10</button>
               </ButtonGroup>
 
               <h3>Time Per Round </h3>
               <ButtonGroup className="me-2" aria-label="Second group">
                 
                 
-                <button class="styledbutton2" id="15" onClick={changeTPR}>15s</button><button class="styledbutton2" id="30" onClick={changeTPR}>30s</button><button class="styledbutton2" id="45" onClick={changeTPR}>45s</button>
-                <button class="styledbutton2" id="60" onClick={changeTPR}>60s</button>
+                <button className="styledbutton2" id="15" onClick={changeTPR}>15s</button><button className="styledbutton2" id="30" onClick={changeTPR}>30s</button><button className="styledbutton2" id="45" onClick={changeTPR}>45s</button>
+                <button className="styledbutton2" id="60" onClick={changeTPR}>60s</button>
               </ButtonGroup>
  
           </div> <Link to='../JoinLobby'>    
           <form onSubmit={handleSubmit}>
          
-          <button class='styledbutton3'>Play</button>  
+          <button className='styledbutton3'>Play</button>  
           
           </form></Link>
         </div>
