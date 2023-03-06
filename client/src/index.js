5fa9fe5208eec11af19873a170d18759689dc8f5
g37436kb <kyle.bate@student.manchester.ac.uk>
2023-03-06 11:33:26 +0000

    changed some stuff :)

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
-//import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+import 'bootstrap/dist/css/bootstrap.css';
+//Put any other imports below so that CSS from your
+//components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
