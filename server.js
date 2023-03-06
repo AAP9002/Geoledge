9d870d66ac30b7ae65df98faccd73438c15cfb0c
aap9002 <aap9002@gmail.com>
2023-03-06 11:17:10 +0000

    game state api

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ require('./backend_Files/Game_Loop_Question_Page')(app, connection);
 require('./backend_Files/kevins_backend.js')(app, connection);
 require('./backend_Files/View_Account_API')(app, connection);
 require('./backend_Files/Leaderboards_API')(app, connection);
+require('./backend_Files/Game_State')(app, connection);
 //////////////////////////////////////////////////////
 
 
