6aad2c38ae7fe25d31d3b956a22669472d133faf
aap9002 <aap9002@gmail.com>
2023-03-06 11:33:17 +0000

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@
 // components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
