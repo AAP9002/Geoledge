7f71a551c4c9115fcd059f29779b058e3f131529
Sj-Shen <shengyu.shen@student.manchester.ac.uk>
2023-03-09 22:15:11 +0800

    dh

--- client/src/Pages/Leaderboards/Leaderboard.jsx
+++ client/src/Pages/Leaderboards/Leaderboard.jsx
@@ -7,22 +7,16 @@ import { useState, useEffect } from 'react';
 function LeaderBoard() {
   
 
-  const [Leaderboard, setLeaderboard] = useState(['test']);
-  const [tableRows, setTableRows] = useState([
-    {name: "Player1", score: "430"},
-    {name: "Player2", score: "580"},
-    {name: "Player3", score: "310"},
-    {name: "Player4", score: "640"},
-    {name: "Player5", score: "495"}
-  ]);
+  const [Leaderboard, setLeaderboard] = useState();
+
 
 
   // API Fetch to get all players
   useEffect(() => {
     fetch('/api/leaderboards?sort=wins').then(res => res.json()).then(fetchedData =>{
-      // console.log(fetchedData);
+      console.log(fetchedData);
       setLeaderboard(fetchedData);
-      console.log(LeaderBoard);
+      console.log(Leaderboard);
     });
     }, []);
 
@@ -40,7 +34,7 @@ function LeaderBoard() {
 
 
         <tbody className='myTable'>
-        {tableRows.map((row)=> <tr><td align='center'>{row.name}</td><td align='center'>{row.score}</td></tr>)}
+        {Leaderboard.map((row)=> <tr><td align='center'>{row.username}</td><td align='center'>{row.wins}</td></tr>)}
         </tbody>
         </table>
         
