557354c54c3fa6634211689d075639bd2bd86bf6
g37436kb <kyle.bate@student.manchester.ac.uk>
2023-03-12 15:33:55 +0000

    change1

--- client/src/Pages/Gameloop/searchbar.jsx
+++ client/src/Pages/Gameloop/searchbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import "./Question.css";
 import { useState, useEffect } from 'react';
 
+
+
+
+const Searchbar = () => {
+
+const [loading, setloading] = useState(true);
+const [countryNames, setCountryNames] = useState([]);
+    
 useEffect(() => {
     fetch('/api/countryNames').then(res => res.json()).then(names =>{
         setCountryNames(names);
         setloading(false);
+        console.log(names);
     });
 }, []);
 
 
-const Searchbar = () => {
-console.log(names)
 }
 
 
