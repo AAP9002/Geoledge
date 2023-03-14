8541ca80d27d6dbabcb8654c201023e223e9b651
Sj-Shen <shengyu.shen@student.manchester.ac.uk>
2023-03-15 02:01:26 +0800

    leaderboard&endofgame

--- client/src/Pages/Gameloop/End_game/End_game.jsx
+++ client/src/Pages/Gameloop/End_game/End_game.jsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import "./End_game.css";
+import React, { useEffect } from 'react';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+ 
 
 
-const End_game = () => {
-    return(<><p>End game</p></>);
+function ScoreBoard(){
+  const [Points, SetPoints] = useState(0)
+  const [Place, SetPlace] = useState(0)
+  const[Username, SetUsername] = useState("Username")
 
+
+  
+  return (<div className='wrapper'>
+          <h1>RankingList</h1>
+          <table className='board'>
+          <thead className='WinsHead'>
+          <tr>
+              <th>Place</th>
+              <th>Player</th>
+              <th>Points</th>
+          </tr>
+      </thead>
+      {ScoreBoard.map((row)=> <tr><td className='data'>{row.Place}</td><td className='data' align='center'>{row.username}</td><td className='data' align='center'>{row.Points}</td></tr>)}
+      </table>
+
+
+       <button onClick={""}>back to current room</button>
+       <button onClick={""}>Back to Home</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
+    )
+
+}
+
+const About = () => {
+  return (
+    <div>
+          <div>
+            
+            <ScoreBoard/>
+          
+          </div>
+    </div>
+ 
+  );
 };
 
-export default End_game;
+export default About;
