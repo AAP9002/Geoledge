ce28ebc069eeadbd789bb14c71ccd1fca6025754
brickchairduck <kevinyeeli004@gmail.com>
2023-03-17 23:15:32 +0000

    "indent"

--- client/src/Pages/Gameloop/End_game/End_game.jsx
+++ client/src/Pages/Gameloop/End_game/End_game.jsx
@@ -6,63 +6,63 @@ import { useState, useEffect } from 'react';
 
 
 function FinishBoard() {
-  
-
-  const [Finishboard, setFinishboard] = useState([{no:"1",username:"fdfdf", scores:"200"},{no:"1",username:"fdfdf", scores:"200"},{no:"2",username:"asass", scores:"100"}
-  ,{no:"3",username:"jkjkj", scores:"50"}]);
 
 
- 
+    const [Finishboard, setFinishboard] = useState([{ no: "1", username: "fdfdf", scores: "200" }, { no: "1", username: "fdfdf", scores: "200" }, { no: "2", username: "asass", scores: "100" }
+        , { no: "3", username: "jkjkj", scores: "50" }]);
 
 
 
-   function returnToHome() {
-    window.location.href = "/#/Home";
-   }
- 
-
-  return (
-    
-    
-   
-    <div className='wrapper'>
-      <h1>Game set!!</h1>
-      <table className='Ranking'>
-        <thead>
-        <tr>
-            <th className='columns'>No</th>
-          <th className='columns'>Player(Username)</th>
-          <th className='columns'>Points</th>
-        </tr>
-        </thead>
-        
 
 
-        <tbody className='RankingTable'>
-        {Finishboard.map((row)=> <tr><td className='data'>{row.no}</td><td className='data'>{row.username}</td><td className='data'>{row.scores}</td></tr>)}
-        </tbody>
-        </table>
-        <button onClick={ returnToHome }>Back to Home</button>
-        
-        
-        
+
+    function returnToHome() {
+        window.location.href = "/#/Home";
+    }
+
+
+    return (
+
+
+
+        <div className='wrapper'>
+            <h1>Game set!!</h1>
+            <table className='Ranking'>
+                <thead>
+                    <tr>
+                        <th className='columns'>No</th>
+                        <th className='columns'>Player(Username)</th>
+                        <th className='columns'>Points</th>
+                    </tr>
+                </thead>
+
+
+
+                <tbody className='RankingTable'>
+                    {Finishboard.map((row) => <tr><td className='data'>{row.no}</td><td className='data'>{row.username}</td><td className='data'>{row.scores}</td></tr>)}
+                </tbody>
+            </table>
+            <button onClick={returnToHome}>Back to Home</button>
+
+
+
         </div>
-      
 
-  )
+
+    )
 }
 
 const About = () => {
-  return (
-    <div>
-          <div>
-            
-            <FinishBoard/>
-          
-          </div>
-    </div>
- 
-  );
+    return (
+        <div>
+            <div>
+
+                <FinishBoard />
+
+            </div>
+        </div>
+
+    );
 };
 
 
