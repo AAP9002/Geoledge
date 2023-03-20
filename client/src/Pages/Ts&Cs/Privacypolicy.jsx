df7f80cdd6db15888b1628544751dfe16d6fe52f
g37436kb <kyle.bate@student.manchester.ac.uk>
2023-03-20 16:17:51 +0000

    privacy policy added

--- client/src/Pages/Ts&Cs/Privacypolicy.jsx
+++ client/src/Pages/Ts&Cs/Privacypolicy.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-          <div>
-            <p>
-              PrivacyPolicy
-            </p>
-          </div>
+    <div className='terms-and-conditions'>
+    <h1>Privacy Policy</h1>
+    <p>Welcome to the Geoledge. By accessing or using our game, you agree to have read the following Privacy Policy:</p>
+    <h2>Data Collection</h2>
+    <p>The only personal data collected is your email address, along with your geoledge password.</p>
+    <h2>Data Usage</h2>
+    <p>Collected data will not be accessible to anyone outside of the geoledge developers, and such data will only be used for the purpose of account identification</p>
+    <h2>Third Party Usage</h2>
+    <p>Collected data will not be shared with any third party sources.</p>
+    
     </div>
  
   );
