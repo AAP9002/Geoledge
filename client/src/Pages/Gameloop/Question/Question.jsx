3553c9326f90c5b35ebbbb6272b2c44c90e0e3ea
g37436kb <kyle.bate@student.manchester.ac.uk>
2023-03-21 07:26:33 +0000

    fixed the searchbar

--- client/src/Pages/Gameloop/Question/Question.jsx
+++ client/src/Pages/Gameloop/Question/Question.jsx
@@ -127,7 +127,7 @@ const Question = (props) => {
                     {countryNames.filter((item) => {
                         const searchTerm = value.toLowerCase();
                         const country = item.country_name.toLowerCase();
-                        return (searchTerm && country.startsWith(searchTerm) && country !== searchTerm);
+                        return (searchTerm && country.startsWith(searchTerm) && country);
                     })
                         .slice(0, 10)
                         .map((item) => (
