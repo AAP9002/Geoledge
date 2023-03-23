7d67c712399e39095a398ff30c67fcea9bafefe2
brickchairduck <kevinyeeli004@gmail.com>
2023-03-23 20:30:15 +0000

    fixed alex (ur code drove me crazy) (ง •̀_•́)ง

--- client/src/Components/signupButton.jsx
+++ client/src/Components/signupButton.jsx
@@ -20,7 +20,7 @@ export const Button = ({
      const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
      return(
-        <Link to='/log-in' className="btn-mobile">
+        <Link to='/sign-up' className="btn-mobile">
             <button
             className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
