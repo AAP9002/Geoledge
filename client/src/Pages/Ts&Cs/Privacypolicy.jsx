2b591b768615e3c86c83e8774971ba05891448cd
Ibra-beh <ibrahim.elbehisy@student.manchester.ac.uk>
2023-03-23 14:35:56 +0000

    changes

--- client/src/Pages/Ts&Cs/Privacypolicy.jsx
+++ client/src/Pages/Ts&Cs/Privacypolicy.jsx
@@ -5,13 +5,13 @@ const About = () => {
   return (
     <div className='terms-and-conditions'>
     <h1>Privacy Policy</h1>
-    <p>Welcome to the Geoledge. By accessing or using our game, you agree to have read the following Privacy Policy:</p>
+    <p style={{ color:"white" }}>Welcome to the Geoledge. By accessing or using our game, you agree to have read the following Privacy Policy:</p>
     <h2>Data Collection</h2>
-    <p>The only personal data collected is your email address, along with your geoledge username and password.</p>
+    <p style={{ color:"white" }}>The only personal data collected is your email address, along with your geoledge username and password.</p>
     <h2>Data Usage</h2>
-    <p>Collected data, with the exception of your username, will not be accessible to anyone outside of the geoledge developers, and such data will only be used for the purpose of account identification</p>
+    <p style={{ color:"white" }}>Collected data, with the exception of your username, will not be accessible to anyone outside of the geoledge developers, and such data will only be used for the purpose of account identification</p>
     <h2>Third Party Usage</h2>
-    <p>Collected data will not be shared with any third party sources.</p>
+    <p style={{ color:"white" }}>Collected data will not be shared with any third party sources.</p>
 
     </div>
  
--- client/src/Pages/Ts&Cs/TermsandConditions.jsx
+++ client/src/Pages/Ts&Cs/TermsandConditions.jsx
@@ -6,19 +6,19 @@ const TermsAndConditions = () => {
   return (
     <div className="terms-and-conditions">
     <h1>Terms and Conditions</h1>
-    <p>Welcome to the Geoledge. By accessing or using our game, you agree to the following terms and conditions:</p>
+    <p style={{ color:"white" }}>Welcome to the Geoledge. By accessing or using our game, you agree to the following terms and conditions:</p>
     <h2>1. Use of the Service</h2>
-    <p>The Geography Quiz Game is provided for entertainment and educational purposes only. You may use the service for personal, non-commercial use only.</p>
+    <p style={{ color:"white" }}>The Geography Quiz Game is provided for entertainment and educational purposes only. You may use the service for personal, non-commercial use only.</p>
     <h2>2. User Conduct</h2>
-    <p>You agree not to use the service for any unlawful or unauthorized purpose. You also agree not to use the game to Post or transmit any content that is harmful or offensive.</p>
+    <p style={{ color:"white" }}>You agree not to use the service for any unlawful or unauthorized purpose. You also agree not to use the game to Post or transmit any content that is harmful or offensive.</p>
     <h2>3. Intellectual Property</h2>
-    <p>The content available in the game, including text, graphics, logos, images, and software, is the property of the Geoledge and is protected by copyright and other laws.</p>
+    <p style={{ color:"white" }}>The content available in the game, including text, graphics, logos, images, and software, is the property of the Geoledge and is protected by copyright and other laws.</p>
     <h2>4. Limitation of Liability</h2>
-    <p>Geoledge is not liable for any damages or losses that may be caused from inconvenient use of the service.</p>
+    <p style={{ color:"white" }}>Geoledge is not liable for any damages or losses that may be caused from inconvenient use of the service.</p>
     <h2>5. Updates and changes</h2>
-    <p>Geoledge holds the right to change any of its terms and conditions with accordance to user laws .</p>
+    <p style={{ color:"white" }}>Geoledge holds the right to change any of its terms and conditions with accordance to user laws .</p>
     <h2>6. Governing Law</h2>
-    <p>These terms and conditions are governed by and construed in accordance with the laws of British Computer Society.</p>
+    <p style={{ color:"white" }}>These terms and conditions are governed by and construed in accordance with the laws of British Computer Society.</p>
   </div>
   );
 };
